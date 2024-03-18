40b97a36075f7a022a8251501ec65d39d134fec0
Rishabh Gupta <imailrishabhgupta@gmail.com>
2024-03-18 11:10:06 +0530

    Refactor: CTA Section mobile optimized

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -2,21 +2,21 @@ import { Button } from "./ui/button";
 
 const CTA = () => {
   return (
-    <div className="py-24 -mt-1">
+    <div className="py-12 -mt-1 sm:py-24">
       <div className="flex bg-[url(/assets/images/CardBG.png)] bg-cover bg-no-repeat bg-center justify-between w-[90%] mx-auto rounded-3xl text-white py-16 shadow-md">
         <div className="flex flex-col items-center justify-center w-full gap-6">
           <h1 className="text-sm uppercase text-gray-50">
             Get started with Supahub for free
           </h1>
-          <h2 className="w-2/4 text-4xl font-semibold text-center">
+          <h2 className="w-[90%] text-2xl font-semibold text-center  sm:w-2/4 sm:text-4xl">
             Ready to revolutionize your approach to machine learning?
           </h2>
-          <p className="w-2/5 font-thin text-center">
+          <p className="w-[90%] sm:w-2/5 font-thin text-center">
             Join the AI4ALL community and experience the future of machine
             learning – accessible, intuitive, and empowering.
           </p>
 
-          <div className="flex gap-4">
+          <div className="flex flex-col gap-4 sm:flex-row">
             <Button className="px-6 text-black bg-white hover:bg-slate-50 hover:text-black">
               <p> Sign up for free</p>
             </Button>
@@ -25,7 +25,7 @@ const CTA = () => {
             </Button>
           </div>
 
-          <div className="flex gap-4 -mt-3 text-sm text-gray-500">
+          <div className="flex flex-col items-center gap-4 text-sm text-gray-500 sm:-mt-3 sm:flex-row">
             <div className="flex gap-1">
               <img
                 className="scale-90"
