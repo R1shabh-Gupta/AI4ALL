1dd2647dedad77b17f53e032615351988c320d93
Rishabh Gupta <imailrishabhgupta@gmail.com>
2024-03-18 11:35:15 +0530

    Refactor: Hero section mobile optimized

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,17 +3,19 @@ import { Button } from "./ui/button";
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center gap-6 mt-24">
-      <h1 className="text-6xl text-[#770785] font-heading font-bold">AI4ALL</h1>
-      <h2 className="w-1/3 text-4xl font-semibold text-center">
+      <h1 className="sm:text-6xl text-5xl text-[#770785] font-heading font-bold">
+        AI4ALL
+      </h1>
+      <h2 className="text-3xl font-semibold text-center sm:w-1/3 sm:text-4xl">
         Democratizing Machine Learning for Everyone
       </h2>
-      <p className="w-2/5 font-thin text-center">
+      <p className="w-3/4 font-thin text-center sm:w-2/5">
         The revolutionary no-code tool designed to simplify and democratize the
         process of selecting, tuning, and deploying machine learning or deep
         learning models for diverse datasets
       </p>
 
-      <div className="flex gap-4">
+      <div className="flex flex-col gap-4 sm:flex-row">
         <Button className="px-6">
           <div className="flex gap-2">
             <img
@@ -29,7 +31,7 @@ const HeroSection = () => {
         </Button>
       </div>
 
-      <div className="flex gap-4 -mt-3 text-sm text-gray-500">
+      <div className="flex flex-col items-center gap-4 -mt-3 text-sm text-gray-500 sm:flex-row">
         <div className="flex gap-1">
           <img
             className="scale-90"
@@ -49,7 +51,7 @@ const HeroSection = () => {
         </div>
       </div>
 
-      <div className="-mt-36 -z-10">
+      <div className="-mt-10 sm:-mt-36 -z-10">
         <img src="/assets/images/herobg.png" alt="hero" />
       </div>
     </div>
